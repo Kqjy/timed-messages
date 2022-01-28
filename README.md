e3796b0fea1e1d949fad347bac00a99f338406ef
NameTheCat <19609995+NameTheCat@users.noreply.github.com>
2022-01-29 01:13:11 +0800

    Add files

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# timed-messages
-For Discord webhooks/anything that requires time
+# Timed messages
+
+For Discord webhooks or anything that requires time.
+
+`noon-progress.py` sends a message at noon at the machine's time, showing how much of the year has passed.
+
+## Setting up
+
+1. Create a [Discord webhook](https://support.discord.com/hc/articles/228383668).
+1. Create `config.py` in the same folder as `noon-progress.py`. Paste in the following code and replace URL with the link of the Discord webhook. This will be the webhook that will be used.
+    ```python
+    webhook_url = 'URL'
+    ```
+1. Run the code daily. On Linux, [cron jobs](https://www.freecodecamp.org/news/cron-jobs-in-linux/) can be used.
+
+## Ideas for future development
+* Filled icon showing how much of the year is over
+* Scheduling message to be sent, rather than `time.sleep()`
+
+## Acknowledgements
+
+Inspired by [@ProgressBar202_](https://twitter.com/ProgressBar202_) on Twitter
---
+++ noon-progress.py
@@ -0,0 +1,33 @@
+import requests
+from datetime import datetime # https://docs.python.org/3/library/datetime.html
+import math
+
+# import secrets
+import config
+
+# returns time in local timezone
+now = datetime.now()
+nowposix = now.timestamp()
+
+# returns next noon in local timezone
+if now.hour < 12: # same day
+    noon = datetime(now.year, now.month, now.day, 12, 00, 00)
+else: # next day
+    noon = datetime(now.year, now.month, now.day + 1, 12, 00, 00)
+noonposix = math.floor(noon.timestamp())
+
+# get percentage complete
+startofyearposix = datetime(now.year, 1, 1).timestamp()
+nextyearposix = datetime(now.year+1, 1, 1).timestamp()
+percentage = (noonposix - startofyearposix) / (nextyearposix - startofyearposix)
+
+# webhook to post to Discord
+# https://discord.com/developers/docs/reference#message-formatting
+payload_start = {
+    'content': f'It is now <t:{noonposix}:f>. {now.year} is {percentage*100}% complete.',
+}
+
+# wait until it is time to post
+time.sleep(noonposix - nowposix)
+
+r = requests.post(config.webhook_url, data=payload_start)
